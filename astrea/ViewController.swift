a7c332a470b14752148fe42ae0f07976bca6b5ff
Ricardo Arango Betancur <dakast24@gmail.com>
2015-10-09 20:56:59 -0500

    Initial Commit

---
+++ astrea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  astrea
+//
+//  Created by Ricardo Arango betancur on 9/10/15.
+//  Copyright © 2015 Ideas App Colombia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
